d6de36e522f4fe328894dacc5137217331431e26
Evangeline Anggana <evangelineanggana@student.laverdad.edu.ph>
2025-08-25 19:18:53 +0800

    Activity 10: Add React About Me project

---
+++ newReact/10_getting-started-react-–/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExZHNqajV5cjVqNHpqa3BlaWx4bXA1a2dpbGxuNjBnOGY2MHFoeWVkbiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/Ke4nRPZNtFQO7dRANF/giphy.gif" target="_blank">
+          <img
+  src="https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExZHNqajV5cjVqNHpqa3BlaWx4bXA1a2dpbGxuNjBnOGY2MHFoeWVkbiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/Ke4nRPZNtFQO7dRANF/giphy.gif"
+  className="logo"
+  alt="Hanabi"
+/>
+      </a>
+        
+      </div>
+      <h3>Name: Evangeline B. Anggana</h3>
+      <h6>Course/Year: BS Information Systems, 3rd Year</h6>
+<h6>Fun Fact: I can cook unfamiliar dishes/viands just by watching a tutorial once.</h6>
+      
+      <p className="read-the-docs">
+        I want to learn React because they'll be essential for me and my progress of learning. Plus, it's where our first semester will be all about so, despite that me and coding don't get along, I have to study this one.
+      </p>
+    </>
+  )
+}
+
+export default App
