4aadcdc00989a7dc828a7a035eceb73916ebab99
Evangeline Anggana <evangelineanggana@student.laverdad.edu.ph>
2025-09-06 11:53:49 +0800

    Add variables activity

---
+++ 11_javascript-refresher/02_variables.js
@@ -0,0 +1,16 @@
+let name = "Eva";
+let age = 20;
+let isStudent = true;
+
+console.log(typeof name);   
+console.log(typeof age);       
+console.log(typeof isStudent);
+
+let a = 10, b = 5;
+console.log(a + b); 
+console.log(a - b);
+console.log(a * b); 
+console.log(a / b); 
+
+console.log("5" == 5);  
+console.log("5" === 5); 
