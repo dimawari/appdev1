890dbbde5f43fdc1844633da1c3d5a7a6235511e
Evangeline Anggana <evangelineanggana@student.laverdad.edu.ph>
2025-09-21 08:59:04 +0800

    Done responding to events / using hooks

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -93,3 +93,32 @@ export default function ShoppingList() {
   );
 }
 
+
+
+    export default function App() {
+    function MyButton() {
+      function handleClick() {
+        alert('You clicked me!');
+      }
+
+      return (
+        <button onClick={handleClick}>
+          Click me
+        </button>
+      );
+    }
+   }
+
+   function MyButton() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
+  return (
+    <button onClick={handleClick}>
+      Clicked {count} times
+    </button>
+  );
+}
