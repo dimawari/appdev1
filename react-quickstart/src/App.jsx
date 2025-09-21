5a077d45e0958d14dc2d82713f72fce50abe69f3
Evangeline Anggana <evangelineanggana@student.laverdad.edu.ph>
2025-09-21 09:01:12 +0800

    Done sharing data between components

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -122,3 +122,20 @@ export default function ShoppingList() {
     </button>
   );
 }
+
+
+export default function MyApp() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+	setCount(count + 1);
+  }
+
+  return (
+	<div>
+  	<h1>Counters that update together</h1>
+  	<MyButton count={count} onClick={handleClick} />
+  	<MyButton count={count} onClick={handleClick} />
+	</div>
+  );
+}
