5b4162b54a2eb55334c861671e1e963a0224869c
Evangeline Anggana <evangelineanggana@student.laverdad.edu.ph>
2025-10-24 21:23:56 +0800

    Done with how to avoid confusing bugs by keeping components pure

--- react-describing-ui/src/App.js
+++ react-describing-ui/src/App.js
@@ -23,3 +23,17 @@ export default function List() {
     </article>
   );
 }
+
+function Cup({ guest }) {
+  return <h2>Tea cup for guest #{guest}</h2>;
+}
+
+export default function TeaSet() {
+  return (
+    <>
+      <Cup guest={1} />
+      <Cup guest={2} />
+      <Cup guest={3} />
+    </>
+  );
+}
