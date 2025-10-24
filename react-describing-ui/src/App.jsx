69da7eada3c68c9ee69a1ff1f47fb2a70fb191f4
Evangeline Anggana <evangelineanggana@student.laverdad.edu.ph>
2025-10-24 21:02:22 +0800

    Done with how to add markup to JavaScript with JSX

--- react-describing-ui/src/App.jsx
+++ react-describing-ui/src/App.jsx
@@ -22,3 +22,21 @@ export default function Gallery() {
     </section>
   );
 }
+
+<>
+  export default function TodoList() {"{"}
+  return ( &lt;&gt;
+  <h1>Hedy Lamarr's Todos</h1>
+  <img
+    src="https://i.imgur.com/yXOvdOSs.jpg"
+    alt="Hedy Lamarr"
+    classname="photo"
+  />
+  <ul>
+    <li>Invent new traffic lights</li>
+    <li>Rehearse a movie scene</li>
+    <li>Improve spectrum technology</li>
+  </ul>
+  );
+  {"}"}
+</>
