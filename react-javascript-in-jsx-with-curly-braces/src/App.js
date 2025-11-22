53c9807fe33bea4184c134800e2ce39cb3160d79
Evangeline Anggana <evangelineanggana@student.laverdad.edu.ph>
2025-11-22 10:17:11 +0800

    done with using curly braces: A window into the JavaScript world

--- react-javascript-in-jsx-with-curly-braces/src/App.js
+++ react-javascript-in-jsx-with-curly-braces/src/App.js
@@ -22,3 +22,20 @@ export default function TodoList() {
     </ul>
   );
 }
+
+
+const today = new Date();
+
+function formatDate(date) {
+  return new Intl.DateTimeFormat(
+    'en-US',
+    { weekday: 'long' }
+  ).format(date);
+}
+
+export default function TodoList() {
+  return (
+    <h1>My Schedule for {formatDate(today)}</h1>
+  );
+}
+
